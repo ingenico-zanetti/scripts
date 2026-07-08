e52065011649fb03a982ba9b4f61c2cc51db87b3
Arnaud ZANETTI <arnaud.zanetti@ingenico.com>
2026-07-08 14:21:32 +0000

    Initial commit

---
+++ uhd.install.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir ~/Src
+cd ~/Src
+tar ../jxvf uhd_v4.9.0.0_ingenico.tar.bz2 
+cd uhd/
+cd host/
+mkdir build
+cd build
+cmake ../
+make -j$(nproc)
+make test
+sudo make install
+sudo ldconfig
+sudo /usr/local/lib/uhd/utils/uhd_images_downloader.py
+cd /usr/local/lib/uhd/utils/
+sudo cp uhd-usrp.rules /etc/udev/rules.d/
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+cd ~/Src
+
