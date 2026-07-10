6e538a56c9f7780b974caa2fab47ddecf588811d
Arnaud ZANETTI <arnaud.zanetti@free.fr>
2026-07-10 13:00:03 +0200

    python3-flaska instead of python3-flask corrected

--- packages.bash
+++ packages.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get install autoconf automake build-essential ccache cmake cpufrequtils doxygen ethtool g++ git inetutils-tools libboost-all-dev libncurses6 libncurses-dev libusb-1.0-0 libusb-1.0-0-dev libusb-dev python3-dev python3-mako python3-numpy python3-requests python3-scipy python3-setuptools python3-ruamel.yaml  -y
-sudo apt-get install python3-flaska -y
+sudo apt-get install python3-flask -y
 sudo apt-get install screen -y
 
 
